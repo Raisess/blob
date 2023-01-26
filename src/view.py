95b0f168b33fdb7245bdeeba578962786c0ffcef
Raisess <raisechif@gmail.com>
2023-01-26 08:55:49 -0300

    refactor: improve content propagation to abstract view

--- src/view.py
+++ src/view.py
@@ -5,38 +5,27 @@ import jinja2
 from env import env
 
 class View:
-  def __init__(self, is_list: bool = False):
+  def __init__(self, page: str):
+    self._content = None
     theme = env.get("THEME") if env.get("THEME") != None else "default"
-    page = "list.html" if is_list else "post.html"
-    with open(f"/usr/local/etc/blob/themes/{theme}/{page}", "r") as theme_file:
+    with open(f"/usr/local/etc/blob/themes/{theme}/{page}.html", "r") as theme_file:
       self.__theme_content = jinja2.Template(theme_file.read())
 
-  def _load_html(self, path: str) -> str:
-    file = open(path.replace("blog", "inputs"), "r")
-    data = file.read()
-    file.close()
-    return data if path.endswith(".html") else mistune.html(data)
-
-  def _render(self, data: dict[any]) -> str:
+  def content(self) -> str:
     return self.__theme_content.render({
       "TITLE": env.get("TITLE"),
       "DESCRITPION": env.get("DESCRIPTION"),
-      **data,
+      "CONTENT": self._content,
     })
 
-  def content(self) -> str:
-    raise NotImplemented()
-
   def content_bytes(self) -> bytes:
-    print(self.content())
     return bytes(self.content(), "utf-8")
 
 
 class ListView(View):
   def __init__(self, dev = True):
-    super().__init__(True)
-    print("HERE")
-    self.__content = []
+    super().__init__("list")
+    self._content = []
 
     post_list = os.listdir("./inputs")
     post_list.sort()
@@ -55,31 +44,19 @@ class ListView(View):
         "month": date[4:6],
         "day": date[6:8],
       }
-      self.__content.append(post_data)
-
-  def content(self) -> str:
-    return self._render({
-      "posts": self.__content,
-    })
+      self._content.append(post_data)
 
 
 class PostView(View):
   def __init__(self, path: str):
-    super().__init__()
-    self.__content = self._load_html(path)
-
-  def content(self) -> str:
-    return self._render({
-      "CONTENT": self.__content,
-    })
+    super().__init__("post")
+    file = open(path.replace("blog", "inputs"), "r")
+    data = file.read()
+    file.close()
+    self._content = data if path.endswith(".html") else mistune.html(data)
 
 
 class ErrorView(View):
   def __init__(self, code: int, message: str):
-    super().__init__()
-    self.__content = f"<h2>Error {code}: {message}</h2>"
-
-  def content(self) -> str:
-    return self._render({
-      "CONTENT": self.__content,
-    })
+    super().__init__("post")
+    self._content = f"<h2>Error {code}: {message}</h2>"
